1684654d95b540375ab47a7fe426f09626f985fb
nodejs <orangexarot@proton.me>
2024-05-30 09:52:15 +0200

    prepping for pub

--- gen.sh
+++ gen.sh
@@ -5,9 +5,6 @@ echo "Starting php server..."
 php -S localhost:8000 > /dev/null 2>&1 & sleep 1
 curl -s localhost:8000/gen.php > index.html
 echo "Changed index.html"
-curl -s localhost:8000/genaudio.php > audio.html
-echo "Changed audio.html"
-
 
 PID=$(pgrep -f "php -S localhost:8000")
 kill $PID
